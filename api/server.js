25f36de212bdff36a71e1ceaabd7cda35b50935f
leeeawa <114284437+leeeawa@users.noreply.github.com>
2023-08-22 12:51:53 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,7 +8,14 @@ import { KeyvFile } from 'keyv-file';
 import ChatGPTClient from '../src/ChatGPTClient.js';
 import ChatGPTBrowserClient from '../src/ChatGPTBrowserClient.js';
 import BingAIClient from '../src/BingAIClient.js';
-
+const cwd = process.cwd(); 
+console.log('Current directory: ' + cwd);
+fs.readdir(cwd, (err, files) => {
+  if (err) throw err;
+  files.forEach(file => {
+    console.log(file);
+  });
+});
 const arg = process.argv.find(_arg => _arg.startsWith('--settings'));
 const path = arg?.split('=')[1] ?? './settings.js';
 
